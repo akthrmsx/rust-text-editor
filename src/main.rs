785996c0804f833ef6041621de48e91f6db65d30
akthrmsx <akt.hrms@gmail.com>
2024-12-06 23:43:52 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use crossterm::terminal;
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    terminal::enable_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(b) => {
+                let c = b as char;
+
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03}\r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+
+    terminal::disable_raw_mode().unwrap();
 }
